f708f9203c8c03300d9637f10bd1b43802902127
Eyck Jentzsch <eyck@minres.com>
2026-07-06 20:49:06 +0200

    remove by alias double defined declaration

--- src/lwtr/lwtr.h
+++ src/lwtr/lwtr.h
@@ -153,14 +153,7 @@ VAL_CONV(sc_core::sc_time);
 template <typename T> value record(T const& t) { return value_converter<T>::to_value(t); }
 
 template <typename T> value record(T& t) { return value_converter<T>::to_value(t); }
-#if !defined(__clang__) && !defined(__APPLE__) && __WORDSIZE == 64
-template <> struct value_converter<sc_dt::uint64> {
-    static value to_value(sc_dt::uint64 v) { return value(static_cast<uint64_t>(v)); }
-};
-template <> struct value_converter<sc_dt::int64> {
-    static value to_value(sc_dt::int64 v) { return value(static_cast<int64_t>(v)); }
-};
-#endif
+
 template <int W> struct value_converter<sc_dt::sc_uint<W>> {
     static value to_value(sc_dt::sc_uint<W> v) { return value(static_cast<uint64_t>(v)); }
 };
